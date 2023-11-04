20c613ae1259a00a97cd3439c974c190fd9b1f0b
Mohammad Maviya Shaikh <maviya9830@gmail.com>
2023-11-04 16:55:24 +0530

    Added the 'Team' model class --> 'model' package.

---
+++ match-hub/src/main/java/com/example/matchhub/models/Team.java
@@ -0,0 +1,42 @@
+package com.example.matchhub.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data // gives setter, getter, and required arg constructor
+// @Data :- also gives required arg constructor, but we have to still initialize the no args and all args
+// constructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "team")
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column(name = "team_name",nullable = false,unique = true)
+    String teamName;
+
+    @Column(name = "fixed_team_size")
+    int fixedTeamSize;
+
+    @OneToOne
+    Player captain;
+
+    @Column(name = "number_of_matches_won",nullable = false)
+    int numberOfMatchesWon;
+
+    @Column(name = "number_of_matches_lost",nullable = false)
+    int numberOfMatchesLost;
+
+    @Column(name = "number_of_matches_played")
+    int numberOfMatchesPlayed;
+
+}
